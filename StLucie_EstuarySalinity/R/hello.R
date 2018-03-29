5e4ceabc65f1b25f11207804c1efc88588e8e7aa
maps-apps-n <mapsandapps.dev@gmail.com>
2018-03-29 12:13:34 -0400

    Update hello.R

--- StLucie_EstuarySalinity/R/hello.R
+++ StLucie_EstuarySalinity/R/hello.R
@@ -5,7 +5,8 @@ library(ggplot2)
 slsalinity <- read.csv("stlucysalinity.csv")
 slsalinity
 # simple time plot 
-plot(slsalinity$date, slsalinity$ppt, type="l", xlab= "Measurements", ylab= "Salinity", col="red" , lwd=2) # measurements in ppt
+plot(slsalinity$date, slsalinity$ppt, type="l", xlab= "Measurements", ylab= "Salinity", col="red" , lwd=2)
+# measurements in ppt
 
 
 
